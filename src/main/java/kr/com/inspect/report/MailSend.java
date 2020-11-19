d0d88d8d8c3dffbc2e07d757ef42b222dd8e0d94
wooyoung <dldndud627@gmail.com>
2020-11-19 11:11:07 +0900

    [2020-11-19 11:10] Send Mail

---
+++ src/main/java/kr/com/inspect/report/MailSend.java
@@ -0,0 +1,42 @@
+package kr.com.inspect.report;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+
+@Service
+public class MailSend {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail(File file, String filename) throws Exception{
+        try{
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+            // 받는 사람
+            messageHelper.setTo("dldndud61@naver.com");
+            // 보내는 사람
+            messageHelper.setFrom("wooyoung.lee@namutech.co.kr");
+            // 메일 제목 (생략 가능)
+            messageHelper.setSubject("메일 전송 테스트");
+            // 메일 본문
+            messageHelper.setText("메일 전송 테스트 입니다.");
+
+            // 파일 첨부
+            FileSystemResource fsr = new FileSystemResource(file);
+            messageHelper.addAttachment(filename, fsr);
+
+            mailSender.send(message);
+
+            file.delete();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
