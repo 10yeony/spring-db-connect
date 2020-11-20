43848f738f137778acde852341ba5a762bec8882
ArkNill <nillwithk@gmail.com>
2020-11-20 14:56:59 +0900

    b
    b

--- src/main/java/kr/com/inspect/controller/ReportController.java
+++ src/main/java/kr/com/inspect/controller/ReportController.java
@@ -1,12 +1,9 @@
 package kr.com.inspect.controller;
 
 import java.util.List;
-
-import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import kr.com.inspect.dto.Member;
 import kr.com.inspect.dto.Utterance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import kr.com.inspect.dto.Metadata;
 import kr.com.inspect.report.DocxReport;
 import kr.com.inspect.report.XlsxReport;
--- src/main/java/kr/com/inspect/dao/MemberDao.java
+++ src/main/java/kr/com/inspect/dao/MemberDao.java
@@ -4,40 +4,96 @@ import java.util.List;
 
 import kr.com.inspect.dto.Member;
 
+/**
+ * 
+ * @author Yeonhee Kim
+ * @version 1.0
+ *
+ */
+
 public interface MemberDao {
-	/* member 가입 */
+	/**
+	 * member 가입
+	 * @param member
+	 * @return
+	 */
 	public int registerMember(Member member);
 	
-	/* 회원 정보 수정 */
+	/**
+	 * 회원 정보 수정
+	 * @param member
+	 * @return
+	 */
 	public int updateMember(Member member);
 	
-	/* 비밀번호 변경 */
+	/**
+	 * 비밀번호 변경
+	 * @param member_id
+	 * @param pwd
+	 * @return
+	 */
 	public int updatePwd(String member_id, String pwd);
 	
-	/* member 탈퇴 */
+	/**
+	 * member 탈퇴
+	 * @param member_id
+	 * @return
+	 */
 	public int deleteMember(String member_id);
 	
-	/* 권한 등록 */
+	/**
+	 * 권한 등록
+	 * @param member_id
+	 * @param authority
+	 * @return
+	 */
 	public int registerAuthority(String member_id, String authority);
 	
-	/* 권한을 모두 삭제 */
+	/**
+	 * 권한을 모두 삭제
+	 * @param member_id
+	 * @return
+	 */
 	public int deleteAuthorities(String member_id);
 	
-	/* 아이디로 회원정보를 읽음 */
+	/**
+	 * 아이디로 회원정보를 읽음
+	 * @param member_id
+	 * @return
+	 */
 	public Member readMemberById(String member_id);
 
-	/* 아이디 중복확인 */
+	/**
+	 * 아이디 중복확인
+	 * @param member_id
+	 * @return
+	 */
 	public int idCheck(String member_id);
 	
-	/* 이메일 중복확인 */
+	/**
+	 * 이메일 중복확인
+	 * @param email
+	 * @return
+	 */
 	public int emailCheck(String email);
 	
-	/* 연락처 중복확인 */
+	/**
+	 * 연락처 중복확인
+	 * @param phone
+	 * @return
+	 */
 	public int phoneCheck(String phone);
 	
-	/* id로 가지고 있는 권한들을 가져옴 */
+	/**
+	 * id로 가지고 있는 권한들을 가져옴
+	 * @param memeber_id
+	 * @return
+	 */
 	public List<String> readAuthorities(String memeber_id);
 
-	/* 회원 정보 모두 가지고옴  */
+	/**
+	 * 회원 정보 모두 가지고옴
+	 * @return
+	 */
 	public List<Member> getMember();
 }
