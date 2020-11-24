5cbc745b53f0e671d943c7a1c9817e79e3035266
wooyoung <dldndud627@gmail.com>
2020-11-24 16:01:48 +0900

    [2020-11-24 16:00] set 10minute watchDir Scheduler

--- src/main/java/kr/com/inspect/scheduler/WatchDir.java
+++ src/main/java/kr/com/inspect/scheduler/WatchDir.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,7 +30,13 @@ public class WatchDir {
      * 감시할 디렉토리 경로
      */
     @Value("${input.json.directory}")
-    private String jsonPath;
+    private String pathFrom;
+
+    /**
+     * 파일을 복사할 디렉토리 경로
+     */
+    @Value("${input.uploadJson.directory}")
+    public String pathTo;
 
     /**
      * PostgreSQL 서비스 필드 선언
@@ -38,76 +48,45 @@ public class WatchDir {
      * 10분마다 디렉토리를 감시하며 json 파일을 DB에 파싱
      * @throws Exception 예외처리
      */
-    @Scheduled(fixedDelay = 3000)
+//    @Scheduled(fixedDelay = 600000)
     public void watchDir() throws Exception {
-//        WatchService watchService = FileSystems.getDefault().newWatchService();
-//
-//        Path path = Paths.get(jsonPath);
-//        path.register(watchService,
-//                StandardWatchEventKinds.ENTRY_CREATE,
-//                StandardWatchEventKinds.ENTRY_DELETE,
-//                StandardWatchEventKinds.ENTRY_MODIFY);
-
-        String day = new SimpleDateFormat("yyyy-MM-dd-ss").format(new Date());
+        String day = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
         System.out.println(day);
 
-//        watchKey = watchService.take();
-//
-//        List<WatchEvent<?>> events = watchKey.pollEvents();
-//
-//        System.out.println(events.size());
+        File dirFrom = new File(pathFrom);
+        File dirTo = new File(pathTo);
+        File[] fileList = dirFrom.listFiles();
 
+        /* json 디렉토리에서 uploadJson 디렉토리로 파일 복사 */
+        for(File file : fileList){
+            File temp = new File(dirTo.getAbsolutePath()+File.separator+file.getName());
 
-//        File dir = new File(path);
-//        File[] fileList = dir.listFiles();
-//        for(File file : fileList){
-//            System.out.println(file.getName());
-//        }
-        // 디렉토리에 변화가 있으면 감지하는 코드
-//        new Thread(()-> {
-//            while (true) {
-//                try {
-//                    watchKey = watchService.take();
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                List<WatchEvent<?>> events = watchKey.pollEvents();
-//
-//                System.out.println(events.size());
-////                if(events != null){
-////                    System.out.println("change");
-////                }
-//
-////                for (WatchEvent<?> event : events) {
-////                    Kind<?> kind = event.kind();
-////
-////                    Path paths = (Path) event.context();
-////
-////                    if (kind.equals(StandardWatchEventKinds.ENTRY_CREATE)) {
-////                        try {
-////                        System.out.println("create " + paths.getFileName() + " in directory ");
-////                            postgreService.insertJSONDir(docxPath);
-////                        } catch (Exception e) {
-////                            e.printStackTrace();
-////                        }
-////                    }
-////                    else if (kind.equals(StandardWatchEventKinds.ENTRY_DELETE)) {
-////                        System.out.println("delete " + paths.getFileName() + " in directory");
-////                    } else if (kind.equals(StandardWatchEventKinds.ENTRY_MODIFY)) {
-////                        System.out.println("modified " + paths.getFileName() + " in directory");
-////                    } else if (kind.equals(StandardWatchEventKinds.OVERFLOW)) {
-////                        System.out.println("OVERFLOW");
-////                    }
-////                }
-////                if (!watchKey.reset()) {
-////                    try {
-////                        watchService.close();
-////                    } catch (IOException e) {
-////                        e.printStackTrace();
-////                    }
-////                }
-//            }
-//        }).start();
+            FileInputStream fis = null;
+            FileOutputStream fos = null;
+
+            try {
+                fis = new FileInputStream(file);
+                fos = new FileOutputStream(temp);
+                byte[] b = new byte[4096];
+                int cnt = 0;
+                while ((cnt=fis.read(b)) != -1){
+                    fos.write(b, 0, cnt);
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            } finally {
+                try {
+                    fis.close();
+                    fos.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            file.delete();
+        }
+
+        /* uploadJson 디렉토리안의 파일을 파싱하고 삭제 */
+        postgreService.insertJSONDir(pathTo);
     }
 }
