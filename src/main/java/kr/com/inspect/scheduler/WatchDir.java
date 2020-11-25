98a64646e25935fba810c325622ca6106cb992fe
wooyoung <dldndud627@gmail.com>
2020-11-25 14:48:36 +0900

    [2020-11-25 14:48]

--- src/main/java/kr/com/inspect/scheduler/WatchDir.java
+++ src/main/java/kr/com/inspect/scheduler/WatchDir.java
@@ -1,6 +1,8 @@
 package kr.com.inspect.scheduler;
 
 import kr.com.inspect.service.PostgreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -25,6 +27,11 @@ import java.util.Date;
 @Component
 @PropertySource(value = "classpath:properties/directory.properties")
 public class WatchDir {
+
+    /**
+     * 로그 출력을 위한 logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(WatchDir.class);
 
     /**
      * 감시할 디렉토리 경로
@@ -52,41 +59,41 @@ public class WatchDir {
     public void watchDir() throws Exception {
         String day = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
-        System.out.println(day);
+        logger.info(day);
 
-        File dirFrom = new File(pathFrom);
-        File dirTo = new File(pathTo);
-        File[] fileList = dirFrom.listFiles();
-
-        /* json 디렉토리에서 uploadJson 디렉토리로 파일 복사 */
-        for(File file : fileList){
-            File temp = new File(dirTo.getAbsolutePath()+File.separator+file.getName());
-
-            FileInputStream fis = null;
-            FileOutputStream fos = null;
-
-            try {
-                fis = new FileInputStream(file);
-                fos = new FileOutputStream(temp);
-                byte[] b = new byte[4096];
-                int cnt = 0;
-                while ((cnt=fis.read(b)) != -1){
-                    fos.write(b, 0, cnt);
-                }
-            } catch (Exception e){
-                e.printStackTrace();
-            } finally {
-                try {
-                    fis.close();
-                    fos.close();
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-            file.delete();
-        }
-
-        /* uploadJson 디렉토리안의 파일을 파싱하고 삭제 */
-        postgreService.insertJSONDir(pathTo);
+//        File dirFrom = new File(pathFrom);
+//        File dirTo = new File(pathTo);
+//        File[] fileList = dirFrom.listFiles();
+//
+//        /* json 디렉토리에서 uploadJson 디렉토리로 파일 복사 */
+//        for(File file : fileList){
+//            File temp = new File(dirTo.getAbsolutePath()+File.separator+file.getName());
+//
+//            FileInputStream fis = null;
+//            FileOutputStream fos = null;
+//
+//            try {
+//                fis = new FileInputStream(file);
+//                fos = new FileOutputStream(temp);
+//                byte[] b = new byte[4096];
+//                int cnt = 0;
+//                while ((cnt=fis.read(b)) != -1){
+//                    fos.write(b, 0, cnt);
+//                }
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    fis.close();
+//                    fos.close();
+//                } catch (IOException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//            file.delete();
+//        }
+//
+//        /* uploadJson 디렉토리안의 파일을 파싱하고 삭제 */
+//        postgreService.insertJSONDir(pathTo);
     }
 }
