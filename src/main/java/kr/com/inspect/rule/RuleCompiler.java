e0d2d40431746e2b759859ed41ae8db84bf738f4
wooyoung <dldndud627@gmail.com>
2020-12-04 16:34:45 +0900

    [2020-12-04 16:34] set properties RuleCompiler

--- src/main/java/kr/com/inspect/rule/RuleCompiler.java
+++ src/main/java/kr/com/inspect/rule/RuleCompiler.java
@@ -2,41 +2,64 @@ package kr.com.inspect.rule;
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 import kr.com.inspect.dto.Rule;
+import org.apache.ibatis.io.Resources;
 
 import javax.tools.*;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.Reader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class RuleCompiler {
 
-    String path = "/home/wooyoung/github/spring-db-connect/src/main/java/";
-    String classPath = "/home/wooyoung/github/spring-db-connect/target/classes/";
+        public RuleCompiler(){
+        String resource = "properties/directory.properties";
+        Properties properties = new Properties();
 
-    // 자바파일 생성하고 컴파일후 class 파일 로드해오는 파일
+        try{
+            Reader reader = Resources.getResourceAsReader(resource);
+            properties.load(reader);
+
+            this.path = properties.getProperty("rule.java.directory");
+            this.classPath = properties.getProperty("rule.class.directory");
+            this.lib = properties.getProperty("rule.lib.directory");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    String path;
+    String classPath;
+    String lib;
+
+    // 자바파일 생성
     public void create(String body) throws Exception {
         // Source를 만들고 Java파일 생성
-        File sourceFile = new File(path + "kr/com/inspect/rule/Test.java");
+        File sourceFile = new File(path + "Test.java");
         String source = getSource(body);
         new FileWriter(sourceFile).append(source).close();
+    }
 
+    // java파일을 읽어와서 compile하여 class파일로 만든 후에 실행
+    public Object runObject() throws Exception {
         // java파일 컴파일 할때 옵션주기
         List<String> optionList = new ArrayList<>();
         // CLASS PATH 추가
         optionList.add("-classpath");
-        optionList.add(System.getProperty("java.class.path"));
+        optionList.add(System.getProperty("java.class.path")+":"+classPath);
         // CLASS 파일 저장할 디렉토리
         optionList.add("-d");
         optionList.add(classPath);
 
         // 만들어진 Java 파일을 컴파일
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        List<String> sources = Arrays.asList(new String[]{path + "kr/com/inspect/rule/Test.java"});
+        List<String> sources = Arrays.asList(new String[]{path + "Test.java"});
         DiagnosticCollector<JavaFileObject> diagnostic = new DiagnosticCollector<JavaFileObject>();
         StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostic, null, null);
         Iterable<? extends JavaFileObject> compilationUnit
@@ -50,13 +73,9 @@ public class RuleCompiler {
                 compilationUnit
         );
         Boolean success = task.call();
-    }
-
-    // Test.class 안의 runMethod 메서드 실행
-    public Object runObject() throws Exception {
 
         // Class 파일 Load
-        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new File(classPath).toURI().toURL(),new File("/opt/tomcat/resources/input/rule/").toURI().toURL()});
+        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{new File(classPath).toURI().toURL(),new File(lib).toURI().toURL()});
         Class<?> cls = classLoader.loadClass("kr.com.inspect.rule.Test");
 
         // 가져온 Class 파일에서 메서드 실행
