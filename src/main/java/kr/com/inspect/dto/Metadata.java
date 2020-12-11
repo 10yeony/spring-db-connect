08cfdd0714327b865b57a5a0c9181d8a082abb13
ArkNill <nillwithk@gmail.com>
2020-12-11 17:06:57 +0900

    a
    a

--- src/main/java/kr/com/inspect/dto/Metadata.java
+++ src/main/java/kr/com/inspect/dto/Metadata.java
@@ -117,134 +117,236 @@ public class Metadata extends CommonDto {
 		this.utterance = utterance;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getRow_num() {
 		return row_num;
 	}
-
+	
+	/**
+	 * 
+	 * @param row_num
+	 */
 	public void setRow_num(int row_num) {
 		this.row_num = row_num;
 	}
-
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getId() {
 		return id;
 	}
-
+	/**
+	 * 
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getCreator() {
 		return creator;
 	}
-
+	/**
+	 * 
+	 * @param creator
+	 */
 	public void setCreator(String creator) {
 		this.creator = creator;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getAnnotation_level() {
 		return annotation_level;
 	}
-
+	/**
+	 * 
+	 * @param annotation_level
+	 */
 	public void setAnnotation_level(String annotation_level) {
 		this.annotation_level = annotation_level;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getYear() {
 		return year;
 	}
-
+	/**
+	 * 
+	 * @param year
+	 */
 	public void setYear(String year) {
 		this.year = year;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getSampling() {
 		return sampling;
 	}
-
+	/**
+	 * 
+	 * @param sampling
+	 */
 	public void setSampling(String sampling) {
 		this.sampling = sampling;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getTitle() {
 		return title;
 	}
-
+	/**
+	 * 
+	 * @param title
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getCategory() {
 		return category;
 	}
-
+	/**
+	 * 
+	 * @param category
+	 */
 	public void setCategory(String category) {
 		this.category = category;
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getAudio_type() {
 		return audio_type;
 	}
-
+	/**
+	 * 
+	 * @param audio_type
+	 */
 	public void setAudio_type(String audio_type) {
 		this.audio_type = audio_type;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getDistributor() {
 		return distributor;
 	}
-
+	/**
+	 * 
+	 * @param distributor
+	 */
 	public void setDistributor(String distributor) {
 		this.distributor = distributor;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public String getRelation() {
 		return relation;
 	}
-
+	/**
+	 * 
+	 * @param relation
+	 */
 	public void setRelation(String relation) {
 		this.relation = relation;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getSentence_count() {
 		return sentence_count;
 	}
-
+	/**
+	 * 
+	 * @param sentence_count
+	 */
 	public void setSentence_count(int sentence_count) {
 		this.sentence_count = sentence_count;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getEojeol_total() {
 		return eojeol_total;
 	}
-
+	/**
+	 * 
+	 * @param eojeol_total
+	 */
 	public void setEojeol_total(int eojeol_total) {
 		this.eojeol_total = eojeol_total;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public Program getProgram() {
 		return program;
 	}
-
+	/**
+	 * 
+	 * @param program
+	 */
 	public void setProgram(Program program) {
 		this.program = program;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public List<Speaker> getSpeaker() {
 		return speaker;
 	}
-
+	/**
+	 * 
+	 * @param speaker
+	 */
 	public void setSpeaker(List<Speaker> speaker) {
 		this.speaker = speaker;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public List<Utterance> getUtterance() {
 		return utterance;
 	}
-
+	/**
+	 * 
+	 * @param utterance
+	 */
 	public void setUtterance(List<Utterance> utterance) {
 		this.utterance = utterance;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	@Override
 	public String toString() {
 		return "Metadata [row_num=" + row_num + ", id=" + id + ", creator=" + creator + ", annotation_level="
