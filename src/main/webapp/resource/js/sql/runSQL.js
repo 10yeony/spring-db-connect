cd9edf7f2e302c07f2ec98ef20df274aa0e12062
wooyoung <dldndud627@gmail.com>
2020-12-30 11:38:49 +0900

    [2020-12-30 11:38] run SQL

--- src/main/webapp/resource/js/sql/runSQL.js
+++ src/main/webapp/resource/js/sql/runSQL.js
@@ -23,11 +23,60 @@ $(function(){
 })
 
 function runSQL(){
-    console.log("runSQL start");
     $.ajax({
         url: contextPath + "/runSQL",
         data: { "query" : myCodeMirror.getValue() },
         type: "POST",
-        async: false
+        async: false,
+
+        success: function (response){
+            var json = JSON.parse(response);
+            var type = json.code;
+
+            if(type=='select'){
+                var list = json.item;
+                var append = "";
+
+                $('#show_result_after_update textarea').empty();
+                for(var i=0; i<list.length; i++){
+                    append += '<table class="table table-bordered paging-table" width="100%" cellspacing="0">';
+                    if(i==0){
+                        append += '<thead><tr>'
+                        for(var j=0; j<list[i].length; j++){
+                            append += '<td>' + list[i][j] + '</td>';
+                        }
+                        append += '</tr></thead>'
+                    }
+                    else if(i==1){
+                        append += '<tbody><tr>';
+                        for(var j=0; j<list[i].length; j++){
+                            append += '<td>' + list[i][j] + '</td>';
+                        }
+                        append += '</tr>';
+                    }
+                    else if(i == list.length-1){
+                        append += '<tr>';
+                        for(var j=0; j<list[i].length; j++){
+                            append += '<td>' + list[i][j] + '</td>';
+                        }
+                        append += '</tr></tbody>';
+                    }
+                    else{
+                        append += '<tr>';
+                        for(var j=0; j<list[i].length; j++){
+                            append += '<td>' + list[i][j] + '</td>';
+                        }
+                        append += '</tr>';
+                    }
+                    append += '</table>';
+                }
+                console.log(append);
+                $('#show_result_after_update textarea').append(append);
+            }
+            else {
+                $('#show_result_after_update textarea').empty();
+                $('#show_result_after_update textarea').append(json.item);
+            }
+        }
     })
 }
