516a62e10d7a3ba23f2d7dec02be741db882560c
yeony <10yeony@gmail.com>
2021-01-14 11:40:55 +0900

    [2021-01-14 11:40] Change method name about chart

--- src/main/java/kr/com/inspect/report/DocxReport.java
+++ src/main/java/kr/com/inspect/report/DocxReport.java
@@ -373,7 +373,7 @@ public class DocxReport {
 					r00.setText("");
 					r00.setFontSize(10);
 					ChartHelper chartHelper = new ChartHelper();
-					doc = chartHelper.addHistogramAboutRuleResult(doc, rule, path);
+					doc = chartHelper.checkChartForm(doc, rule, path);
 				}
 			}
 			// rule의 result가 배열이 아닌경우 1x1 표로 결과 출력
